bc48539e242b5d4ca80397ed2ff5200e90256eff
Abrar74774 <abrarshahriarhossain@gmail.com>
2022-01-07 10:54:44 +0300

    Updates

---
+++ precache-manifest.d30ad32d71c59ea6412ff3e0bf7ce806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd3dc5ccbd39588a316604d04eb6a33",
+    "url": "/CarTunerApp/index.html"
+  },
+  {
+    "revision": "ca427da2816da0ab236f",
+    "url": "/CarTunerApp/static/css/main.40f92c21.chunk.css"
+  },
+  {
+    "revision": "189600ff62e49b997968",
+    "url": "/CarTunerApp/static/js/2.3e56f8be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CarTunerApp/static/js/2.3e56f8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca427da2816da0ab236f",
+    "url": "/CarTunerApp/static/js/main.478f0363.chunk.js"
+  },
+  {
+    "revision": "2d7e5a9c3cc970c8cb0a",
+    "url": "/CarTunerApp/static/js/runtime-main.57efbed2.js"
+  }
+]);
